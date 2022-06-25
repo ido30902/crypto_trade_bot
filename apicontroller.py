820f4f60ecf6184dd29950eb3feb040fc97804b2
ido30902 <idobarabi@gmail.com>
2022-06-25 12:33:23 +0300

    stream manager

--- apicontroller.py
+++ apicontroller.py
@@ -1,11 +1,51 @@
+import json
 from binance.client import Client
 from dotenv import load_dotenv
 import os
+import websocket
 
 class APIController:
+    
     def __init__(self):
         load_dotenv()
         self.client = Client(os.getenv("API_KEY"),os.getenv("SECRET"))
     
+    def get_coin_price(self,coin):
+        return self.client.get_avg_price(symbol=coin)
 
+    def buy_coin(self,coin,amount):
+        return self.client.order_market_buy(symbol=coin, quantity=amount)
+
+    def sell_coin(self,coin,amount):
+        return self.client.order_market_sell(symbol=coin, quantity=amount)
     
+    
+
+class StreamManager:
+    def __init__(self):
+        self.ws = websocket.WebSocketApp(os.getenv('SOCKET') , 
+                    on_message = lambda ws,msg: self.on_message(ws, msg),
+                    on_error   = lambda ws,msg: self.on_error(ws, msg),
+                    on_close   = lambda ws:     self.on_close(ws),
+                    on_open    = lambda ws:     self.on_open(ws))
+
+    def start_stream(self):
+        self.ws.run_forever()
+        print('Stream started')
+
+    def stop_stream(self):
+        self.ws.stop()
+        print('Stream stopped')
+
+    def on_open(self,ws):
+        print('Connection opened')
+
+    def on_close(self,ws):
+        print('Connection closed')
+
+    def on_message(self,ws,message):
+        price = json.loads(message)['k']['c']
+        print(f'price is {price}')
+         
+    def on_error(self,ws,error):
+        print('Error received ' + str(error))
--- main.py
+++ main.py
@@ -1,32 +1,22 @@
 import json
-import websocket
+from apicontroller import *
 
-from apicontroller import APIController
-
-coin = 'LUNAUSDT'
-money = 0
-
+coin = 'XRPUSDT'
 
 def main():
 
     controller = APIController()
+    streamer = StreamManager()
+
+    print(controller.get_coin_price(coin))
+
+    streamer.start_stream()
     
-    ws = websocket.WebSocketApp(os.getenv('SOCKET') , on_open=on_open, on_close=on_close, on_message=on_message, on_error=on_error)
-    ws.run_forever()
-  
-def on_open(ws):
-    print('Connection opened')
-
-def on_close(ws):
-    print('Connection closed')
-
-def on_message(ws,message):
-    pass
+    
     
 
 
-def on_error(ws,error):
-    print('Error received ' + str(error))
+    
 
     
 if __name__ == '__main__':
